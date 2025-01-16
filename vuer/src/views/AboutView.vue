5b440bfcfe29df894986d47ce7b0b16d3a764d98
dyyyylan <jiangdylan00@gmail.com>
2025-01-16 12:30:37 -0500

    a

--- vuer/src/views/AboutView.vue
+++ vuer/src/views/AboutView.vue
@@ -7,12 +7,15 @@
 <input type="password" v-model="user.password">
 <button type="submit">sign in</button>
 </form>
+<h2 v-if="skibidi">Mark Fridlin</h2>
+<h2 v-else>ryan chen</h2>
   </div>
 </template>
 
 <script setup>
 import {reactive} from 'vue'
 const user = reactive({username:'', password:''})
+const skibidi = false
 </script>
 
 <style scoped>
