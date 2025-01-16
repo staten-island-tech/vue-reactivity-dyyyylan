8231f7d6381f81402600c159f6de9931b56872a3
dyyyylan <jiangdylan00@gmail.com>
2025-01-16 12:26:34 -0500

    a

---
+++ vuer/src/components/form.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+<form name="login-form" @submit.prevent="login">
+<label for="username">Enter Username</label>
+<input type="text" v-model="user.username">
+<label for="password">Enter Password</label>
+<input type="password" v-model="user.password">
+</form>
+    </div>
+</template>
+
+<script setup>
+import {reactive} from 'vue'
+const user = reactive({username:'', password:''})
+</script>
+
+<style scoped>
+
+</style>
---
+++ vuer/src/views/AboutView.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <form name="login-form" @submit.prevent="login">
+<label for="username">Enter Username</label>
+<input type="text" v-model="user.username">
+<label for="password">Enter Password</label>
+<input type="password" v-model="user.password">
+<button type="submit">sign in</button>
+</form>
+  </div>
+</template>
+
+<script setup>
+import {reactive} from 'vue'
+const user = reactive({username:'', password:''})
+</script>
+
+<style scoped>
+
+</style>
